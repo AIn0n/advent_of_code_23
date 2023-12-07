7bab7d853e7a7bd0dcebea6da3551ca8608f7fe4
Szymon Górka <55995692+AIn0n@users.noreply.github.com>
2023-12-07 17:13:56 +0100

    Test 3 is green

--- 5day/ranges.py
+++ 5day/ranges.py
@@ -1,6 +1,14 @@
-
 class Range:
-    def __init__(self, start, l) -> None:
+    def __init__(self, start, l, shift=0) -> None:
         self.start = start
         self._len = l
         self.end = self.start + self._len - 1
+        self.shift = shift
+
+    @staticmethod
+    def from_str(s: str) -> "Range":
+        dst, src, l = (int(x) for x in s.split(" ") if x != "")
+        return Range(src, l, shift=dst - src)
+
+    def split(t, o: "Range") -> list["Range"]:
+        return [Range(t.start + o.shift, t._len)]
--- 5day/test_range.py
+++ 5day/test_range.py
@@ -1,9 +1,28 @@
 from ranges import Range
+from pytest import mark
+
 
 def test_constructor_start_end_len_parameters():
     r = Range(79, 14)
-    
+
     assert r.start == 79
     assert r._len == 14
     assert r.end == 92
 
+
+def test_build_from_string_returns_proper_range():
+    r = Range.from_str("45 77 23")
+    assert r.start == 77
+    assert r.shift == (45 - 77)
+    assert r._len == 23
+
+
+def test_split_first_is_smaller_both_sides_returns_shifted_first_range():
+    first = Range(79, 14)
+    second = Range.from_str("52 50 48")
+    res = first.split(second)[0]
+
+    shift = 52 - 50
+    assert res.start == first.start + shift
+    assert res.end == first.end + shift
+    assert res._len == first._len
