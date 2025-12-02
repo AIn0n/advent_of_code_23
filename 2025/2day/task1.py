d7c749719d5547d7bec3d8e2d4032a7d5bf254f6
Szymon Górka <55995692+AIn0n@users.noreply.github.com>
2025-12-02 08:45:19 +0100

    solved 2025 day 2 task 1

--- 2025/2day/task1.py
+++ 2025/2day/task1.py
@@ -1,6 +1,5 @@
 import re
 
-TEST_INPUT = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
 
 def is_repetition(num: int) -> bool:
     seq = ""
@@ -11,7 +10,7 @@ def is_repetition(num: int) -> bool:
     for digit in str_num[:(len_num // 2)]:
         seq += digit
         find_res = re.findall(seq, str_num)
-        if "".join(find_res) == str_num:
+        if "".join(find_res) == str_num and len(find_res) == 2:
             return True
     return False
 
@@ -23,11 +22,10 @@ def solution(inp: str) -> int:
         start, end = el.split("-")
         start, end = int(start), int(end)
         invalid_ids = [el for el in range(start, end + 1) if is_repetition(el)]
-        print(f"({start} - {end}) = {invalid_ids}")
         res.extend(invalid_ids)
     return sum(res)
 
 
 
 if __name__ == "__main__":
-    print(solution(TEST_INPUT))
+    print(solution(open("valid_input.txt").read()))
